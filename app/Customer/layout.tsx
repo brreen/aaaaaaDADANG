e706fc2b3c489a52372d585e709bee15228d18ca
brreen <tsfffy@gmail.com>
2025-06-12 16:48:49 +0700

    evan ganteng

--- app/Customer/layout.tsx
+++ app/Customer/layout.tsx
@@ -1,11 +1,15 @@
 import Navbar from "../../components/Navbar/page";
 import Footer from "../../components/Footer/page";
-
+import { Suspense } from 'react';
 
 export default function CustomerLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen bg-white">
+      {/* <Navbar />
+       */}
+           <Suspense fallback={<div>Loading navbar...</div>}>
       <Navbar />
+    </Suspense>
       <main className="flex-grow">{children}</main>
       <Footer />
     </div>
